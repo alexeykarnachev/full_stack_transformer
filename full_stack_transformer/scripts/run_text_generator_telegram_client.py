f1fd4607af1f273e58fe6cbfb67256c9d2ad1e0f
Alexey Karnachev <alekseykarnachev@gmail.com>
2020-05-05 22:01:07 +0300

    run_text_generator_telegram_client.py script added

---
+++ full_stack_transformer/scripts/run_text_generator_telegram_client.py
@@ -0,0 +1,41 @@
+import argparse
+
+from aiogram.utils import executor
+
+from full_stack_transformer.text_generator_telegram_client.utilities import prepare
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        description='This script runs telegram client for the text generator '
+                    'service')
+
+    parser.add_argument(
+        '--telegram_api_token', type=str, required=True,
+        help='Telegram client API token. Could be obtained via `@BotFather` '
+             'bot in telegram.')
+    parser.add_argument(
+        '--text_generator_service_url', type=str, required=True)
+    parser.add_argument(
+        '--text_generator_service_login', type=str, required=False)
+    parser.add_argument(
+        '--text_generator_service_password', type=str, required=False)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = _parse_args()
+
+    dispatcher = prepare(
+        api_token=args.telegram_api_token,
+        text_generator_service_url=args.text_generator_service_url,
+        text_generator_service_login=args.text_generator_service_login,
+        text_generator_service_password=args.text_generator_service_password)
+
+    executor.start_polling(dispatcher, skip_updates=True)
+
+
+if __name__ == '__main__':
+    main()
