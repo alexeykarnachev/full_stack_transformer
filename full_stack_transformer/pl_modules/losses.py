477aba116693334d674d24ff616676044c40bbd1
Alexey Karnachev <alekseykarnachev@gmail.com>
2020-05-05 16:27:08 +0300

    unlikelihood_candidates_loss added

---
+++ full_stack_transformer/pl_modules/losses.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn.functional as F
+
+
+def unlikelihood_candidates_loss(logits, target):
+    logp = F.log_softmax(logits, 2)
+    seq_len = logits.size()[1]
+    bs = logits.size()[0]
+
+    logp_flat = logp.view(logits.size()[0] * logits.size()[1], -1)
+    tril = torch.tril(torch.ones((seq_len, seq_len), device=logits.device), diagonal=-1)
+
+    cols = target.repeat(1, seq_len).view(seq_len * bs, -1)
+    rows = torch.arange(0, cols.size()[0]).unsqueeze(-1).repeat(1, seq_len)
+
+    not_ignore_mask = ~(cols == target.flatten().unsqueeze(-1).repeat(1, seq_len))
+    candidates_mask = tril.repeat(bs, 1).bool()
+    candidates_mask *= not_ignore_mask
+
+    cols_flat = cols[candidates_mask]
+    rows_flat = rows[candidates_mask]
+
+    candidates_logp = logp_flat[rows_flat, cols_flat]
+
+    probs = torch.clamp((1.0 - candidates_logp.exp()), min=1e-5)
+
+    loss = -torch.log(probs)
+    loss = loss.mean()
+
+    return loss
+
