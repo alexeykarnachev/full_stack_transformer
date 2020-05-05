f72bd414f023012f12dbe5a7a387d885175c988a
Alexey Karnachev <alekseykarnachev@gmail.com>
2020-05-06 00:41:46 +0300

    defaults changed

--- full_stack_transformer/text_generator_service/schemas.py
+++ full_stack_transformer/text_generator_service/schemas.py
@@ -12,9 +12,9 @@ class GeneratedTexts(BaseModel):
 class TextGeneratorAppParams(TextGeneratorParams):
     generation_max_len: int = Field(default=256, ge=1, le=512)
     temperature: float = Field(default=0.7, gt=0, le=100)
-    top_k: int = Field(default=50, ge=0)
+    top_k: int = Field(default=0, ge=0)
     top_p: float = Field(default=1.0, gt=0.0, le=1.0)
-    repetition_penalty: float = Field(default=5.0, ge=1.0, le=100)
+    repetition_penalty: float = Field(default=1.0, ge=1.0, le=100)
     num_return_sequences: int = Field(default=1, ge=1, le=64)
     seed_text: str = Field(default='', max_length=1024)
     ignored_words: Optional[Sequence[str]] = Field(default=[], max_length=1024)
