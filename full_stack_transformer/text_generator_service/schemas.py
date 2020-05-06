891e98038d3bef77f3a0e7627a9f1ae3dd06fb64
Alexey Karnachev <alekseykarnachev@gmail.com>
2020-05-07 02:05:34 +0300

    service defaults changed

--- full_stack_transformer/text_generator_service/schemas.py
+++ full_stack_transformer/text_generator_service/schemas.py
@@ -11,9 +11,9 @@ class GeneratedTexts(BaseModel):
 
 class TextGeneratorAppParams(TextGeneratorParams):
     generation_max_len: int = Field(default=256, ge=1, le=512)
-    temperature: float = Field(default=0.7, gt=0, le=100)
+    temperature: float = Field(default=1.0, gt=0, le=100)
     top_k: int = Field(default=0, ge=0)
-    top_p: float = Field(default=1.0, gt=0.0, le=1.0)
+    top_p: float = Field(default=0.99, gt=0.0, le=1.0)
     repetition_penalty: float = Field(default=1.0, ge=1.0, le=100)
     num_return_sequences: int = Field(default=1, ge=1, le=64)
     seed_text: str = Field(default='', max_length=1024)
