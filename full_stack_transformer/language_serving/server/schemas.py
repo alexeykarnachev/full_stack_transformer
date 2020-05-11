825e365e1104c3f434740c50df32317f1b448b0b
Alexey Karnachev <alekseykarnachev@gmail.com>
2020-05-11 22:33:18 +0300

    app defaults changed

--- full_stack_transformer/language_serving/server/schemas.py
+++ full_stack_transformer/language_serving/server/schemas.py
@@ -12,8 +12,8 @@ class LanguageGeneratorAppParams(BaseModel):
     meta: Optional[str] = Field(default=None, max_length=1024)
 
     max_number_of_generated_tokens: int = Field(default=128, ge=1, le=512)
-    temperature: float = Field(default=1.0, gt=0, le=100)
-    top_k: int = Field(default=0, ge=0)
-    top_p: float = Field(default=0.99, gt=0.0, le=1.0)
-    repetition_penalty: float = Field(default=1.0, ge=1.0, le=100)
+    temperature: float = Field(default=0.7, gt=0, le=100)
+    top_k: int = Field(default=50, ge=0)
+    top_p: float = Field(default=1.0, gt=0.0, le=1.0)
+    repetition_penalty: float = Field(default=5.0, ge=1.0, le=100)
     num_return_sequences: int = Field(default=1, ge=1, le=64)
