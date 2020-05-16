fee777b55e1ae0ec090224bf0893dd8f00fd0351
Alexey Karnachev <alekseykarnachev@gmail.com>
2020-05-16 14:49:19 +0300

    modeli input cuda method

--- full_stack_transformer/core/model_input.py
+++ full_stack_transformer/core/model_input.py
@@ -5,8 +5,15 @@ class ModelInputError(Exception):
     pass
 
 
-ModelInput = namedtuple(
-    typename='ModelInput',
-    field_names=('input_ids', 'token_type_ids', 'lm_labels', 'past'),
-    defaults=(None, None, None, None)
-)
+class ModelInput(namedtuple):
+    def __init__(self):
+        super().__init__(
+            typename='ModelInput',
+            field_names=('input_ids', 'token_type_ids', 'lm_labels', 'past'),
+            defaults=(None, None, None, None)
+        )
+
+    def cuda(self, gpu_id):
+        for name, value in self._asdict().iteritems():
+            if value is not None:
+                self[name] = value.cuda(gpu_id)
