7a8a3a1a19c34591a2480a6376abe084abe7d8f3
Alexey Karnachev <alekseykarnachev@gmail.com>
2020-05-22 14:43:54 +0300

    dialog tokenizer test

--- tests/core/tasks/dialog_decoder/test_tokenizer.py
+++ tests/core/tasks/dialog_decoder/test_tokenizer.py
@@ -1,33 +1,49 @@
-import pytest
-
 from full_stack_transformer.tasks.dialog_decoder.text_input import DialogInput
+from full_stack_transformer.tasks.dialog_decoder.tokenizer import (
+    _PERSONA_SPEAKER,
+    _NOT_PERSONA_SPEAKER,
+    _END_OF_PERSONA,
+    _END_OF_TAGS
+)
 from full_stack_transformer.tasks.dialog_decoder.tokenizer_impl.ru_transformers import \
     RuTransformersDialogTokenizer
 
 
-@pytest.mark.parametrize(
-    'max_tags_len,max_pers_len,max_dialog_len', [
-        (10, 10, 10)
-    ]
-)
-def test_tokenizer(max_tags_len, max_pers_len, max_dialog_len):
+def test_tokenizer():
     tokenizer = RuTransformersDialogTokenizer(
-        max_tags_len=max_tags_len,
-        max_pers_len=max_pers_len,
-        max_dialog_len=max_dialog_len
+        max_tags_len=100,
+        max_pers_len=100,
+        max_dialog_len=100
     )
+
+    utterances = ['ААПП', 'ББББ', 'АВВА']
+    persona = 'П'
+    tags = 'T'
+    persona_idx = 0
 
     text_input = DialogInput(
-        utterances=['Мама мыла раму', 'Где', 'Там'],
-        persona_idx=0,
-        persona='',
-        tags=''
+        utterances=utterances,
+        persona_idx=persona_idx,
+        persona=persona,
+        tags=tags
     )
 
-    encoding = tokenizer.encode_for_inference(text_input=text_input)
+    eos = tokenizer.eos_token
+    expected_decoded_ids = f'{tags}{_END_OF_TAGS} {persona}{_END_OF_PERSONA}'
+    not_pers_tok = _NOT_PERSONA_SPEAKER
+    for i_utt, utt in enumerate(utterances):
+        if (i_utt % 2) == persona_idx:
+            pers_tok, not_pers_tok = _PERSONA_SPEAKER, _NOT_PERSONA_SPEAKER
+        else:
+            pers_tok, not_pers_tok = _NOT_PERSONA_SPEAKER, _PERSONA_SPEAKER
 
-    print('\n')
-    print(tokenizer.decode(encoding.token_ids[-4:]))
-    print('\n')
-    print(tokenizer.decode(encoding.token_type_ids[-4:]))
-    print('\n')
+        expected_decoded_ids += f'{pers_tok} {utt}{eos}'
+
+    encoding = tokenizer.encode_for_train(text_input=text_input)
+    decoded_ids = tokenizer.decode(encoding.token_ids)
+    assert decoded_ids == expected_decoded_ids
+
+    expected_decoded_ids += not_pers_tok
+    encoding = tokenizer.encode_for_inference(text_input=text_input)
+    decoded_ids = tokenizer.decode(encoding.token_ids)
+    assert decoded_ids == expected_decoded_ids
